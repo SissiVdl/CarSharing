a27c62f59fafa9f6e79cf3c996a92d3d539f58b1
svd2vjb <vandenlangenbergh.s@gmail.com>
2024-11-26 17:07:10 +0100

    task: make database connection and create table

--- Car Sharing/task/src/carsharing/Main.java
+++ Car Sharing/task/src/carsharing/Main.java
@@ -1,8 +1,11 @@
 package carsharing;
 
+import carsharing.util.DataBase;
+
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
+        DataBase.createDatabase(args);
     }
 }
