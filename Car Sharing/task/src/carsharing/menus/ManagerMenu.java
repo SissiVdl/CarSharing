f8753f03ccefc4389af7a320acdacde2aee9a752
svd2vjb <vandenlangenbergh.s@gmail.com>
2024-11-28 14:13:48 +0100

    feat: manager menu - Back = the program should print the main menu.

--- Car Sharing/task/src/carsharing/menus/ManagerMenu.java
+++ Car Sharing/task/src/carsharing/menus/ManagerMenu.java
@@ -4,20 +4,20 @@ import carsharing.util.Keyboard;
 
 public class ManagerMenu {
     public void show() {
-        System.out.println("1. Company list");
-        System.out.println("2. Create a company");
-        System.out.println("0. Back");
+        boolean keepGoing = true;
 
-        handleChoice();
-    }
+        while (keepGoing) {
+            System.out.println("1. Company list");
+            System.out.println("2. Create a company");
+            System.out.println("0. Back");
 
-    public void handleChoice() {
-        int choice = Keyboard.getInt();
-        switch (choice) {
-            case 1 -> System.out.println("1. Company list");
-            case 2 -> System.out.println("2. Create a company");
-            case 0 -> System.out.println("0. Back");
-            default -> throw new IllegalStateException("Unexpected value: " + choice);
-        }
+            int choice = Keyboard.getInt();
+            switch (choice) {
+                case 1 -> System.out.println("1. Company list");
+                case 2 -> System.out.println("2. Create a company");
+                case 0 -> keepGoing = false;
+                default -> throw new IllegalStateException("Unexpected value: " + choice);
+            }
+       }
     }
 }
